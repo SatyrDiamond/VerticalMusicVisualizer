83aa719b464c1ae02730288dafee2152bab9f0ca
Colby Ray <colbydray@gmail.com>
2022-03-24 13:43:02 -0400

    Update art_sh.sh

--- art_sh.sh
+++ art_sh.sh
@@ -7,11 +7,11 @@ convert -brightness-contrast -50x-20 -blur 0x15 artcropped.bmp artfinal.bmp
 
 rm final.avi
 
-ffmpeg -i 'Vitas - Зима.flac' -i song.png -i artfinal.bmp -filter_complex " \
-[0:a] showwaves=s=1080x1920:mode=cline:s=1080x1920:colors=#404040@1:scale=sqrt:r=30,format=rgba [waveform]; \
+ffmpeg -i 'Vitas - Зима.flac' -i song.png -i artfinal.bmp -filter_complex "\
+[0:a] showwaves=s=1080x1920:mode=cline:s=1080x1920:colors=#303030@1:scale=sqrt:r=30,format=rgba [waveform]; \
 [1:v] format=rgba [textimage]; \
 [2:v] format=rgba [coverimage]; \
-[coverimage][waveform] blend=all_mode=subtract [image]; \
+[coverimage][waveform] blend=all_mode=subtract,format=rgba [image]; \
 [textimage][image] blend=all_mode=lighten:all_opacity=1,format=rgba" -vb 20M final.avi
 
 rm artcropped.jpg

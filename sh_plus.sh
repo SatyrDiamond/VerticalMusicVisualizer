04bcacff68205346d21847c6388896befe595215
Colby Ray <colbydray@gmail.com>
2022-03-24 13:20:44 -0400

    Update sh_plus.sh

--- sh_plus.sh
+++ sh_plus.sh
@@ -1,10 +1,12 @@
-wkhtmltoimage --width 576 --height 1024 --enable-local-file-access song.html song.png
+wkhtmltoimage --enable-local-file-access --width 1080 --height 1920 --enable-local-file-access song.html song.png
 
-wkhtmltoimage --width 576 --height 1024 --enable-local-file-access from.html from.png
+wkhtmltoimage --enable-local-file-access --width 1080 --height 1920 --enable-local-file-access from.html from.png
 
 convert from.png -transparent '#FF00FF' from2.png
 
-ffmpeg -y -i test.wav -i song.png -i from2.png -filter_complex " \
-[0:a] showwaves=s=576x1024:mode=cline:s=576x1024:colors=#333333@0.9:scale=sqrt:r=30,format=rgba [bg]; \
+# -stream_loop 1
+ffmpeg  -y -i "Module2n.wav" -i song.png -i from2.png -filter_complex " \
+[0:a] showwaves=s=1080x1920:mode=cline:s=1080x1920:colors=#383838@0.9:scale=sqrt:r=30,format=rgba [bg]; \
 [1:v] format=rgba [fg]; \
 [bg][fg] blend=all_mode=lighten:all_opacity=1,format=yuva422p10le[vid]; [vid][2:v]overlay" -vb 20M final.avi
+

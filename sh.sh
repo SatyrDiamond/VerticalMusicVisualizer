63fab7e880957e60a1697c4d9fa16459f417ae50
Colby Ray <colbydray@gmail.com>
2022-03-24 13:20:42 -0400

    Update sh.sh

--- sh.sh
+++ sh.sh
@@ -1,8 +1,8 @@
-wkhtmltoimage --width 576 --height 1024 --disable-smart-width song.html song.png
+wkhtmltoimage --enable-local-file-access --width 1080 --height 1920 --disable-smart-width song.html song.png
 
-rm final.mp4
+rm final.avi
 
-ffmpeg -i TonyThai.flac -i song.png -filter_complex " \
-[0:a] showwaves=s=576x1024:mode=cline:s=576x1024:colors=#222222@0.9:scale=sqrt:r=30,format=rgba [bg]; \
-[1:v] format=rgba [fg]; \
-[bg][fg] blend=all_mode=lighten:all_opacity=1,format=yuva422p10le" final.mp4
+ffmpeg -i 'Module2.s3m' -i song.png -filter_complex " \
+[0:a] showwaves=s=1080x1920:mode=cline:s=1080x1920:colors=#383838@0.9:scale=sqrt:r=30,format=rgba [waveform]; \
+[1:v] format=rgba [textimage]; \
+[waveform][textimage] blend=all_mode=lighten:all_opacity=1,format=yuva422p10le" -vb 20M final.avi
